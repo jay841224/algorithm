4d3e20e15cf7d2dda17938cfc32985790b80d0f6
jay841224 <j1360fy6416nb@gmail.com>
2019-10-31 21:26:16 +0800

    bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+print(bubble_sort([27, 33, 28, 4, 2, 26, 13, 35, 8, 14]))
